4c2cabcc239fb40a89104a6f89212e3bb1debab6
Amir Bin <ur.amirbin@gmail.com>
2021-09-15 13:00:20 +0430

    Implement Plane

--- Assets/UbiRock/Wakizashi/Scripts/Plane.cs
+++ Assets/UbiRock/Wakizashi/Scripts/Plane.cs
@@ -1,5 +1,20 @@
+using UnityEngine;
+
 namespace UbiRock.Toolkit {
     public class Plane {
-        
+        private Vector3 _position;
+        private Vector3 _normal;
+
+        public Plane(Vector3 position, Vector3 normal) {
+            _position = position;
+            _normal = normal;
+        }
+
+        public Plane(Vector3 a, Vector3 b, Vector3 c) {
+            _normal = Vector3.Cross(b - a, c - a);
+            _position = a;
+        }
+
+        public Vector3 Normal { get => _normal; }
     }
 }
