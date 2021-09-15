c0a83cb1b0c8dd9c5664915cf410e377dae81138
Amir Bin <ur.amirbin@gmail.com>
2021-09-15 13:26:33 +0430

    Implement Triangle

--- Assets/UbiRock/Wakizashi/Scripts/Tri.cs
+++ Assets/UbiRock/Wakizashi/Scripts/Tri.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 namespace UbiRock.Wakizashi.Toolkit {
     public class Tri {
-        
+        private Vertex _vertA, _vertB, _vertC;
+        public Tri(Vertex a, Vertex b, Vertex c) {
+            _vertA = a;
+            _vertB = b;
+            _vertC = c;
+        }
     }
 }
