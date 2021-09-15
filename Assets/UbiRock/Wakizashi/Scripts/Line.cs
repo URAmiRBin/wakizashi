fdc069a74aa576c973857be518fbe1be998fd636
Amir Bin <ur.amirbin@gmail.com>
2021-09-15 12:50:44 +0430

    Implement Line

--- Assets/UbiRock/Wakizashi/Scripts/Line.cs
+++ Assets/UbiRock/Wakizashi/Scripts/Line.cs
@@ -1,6 +1,16 @@
+using UnityEngine;
 
 namespace UbiRock.Toolkit {
     public class Line {
-        
+        private Vector3 _pointA, _pointB;
+
+        public Line(Vector3 a, Vector3 b) {
+            _pointA = a;
+            _pointB = b;
+        }
+
+        public Vector3 PointA { get => _pointA; }
+        public Vector3 PointB { get => _pointB; }
+        public Vector3 NormalizedDirection { get => Vector3.Normalize(_pointB - PointA); }
     }
 }
