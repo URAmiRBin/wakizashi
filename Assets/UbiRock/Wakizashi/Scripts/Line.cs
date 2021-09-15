141eedb19c2136b956ced443d69514e7a52e06ad
Amir Bin <ur.amirbin@gmail.com>
2021-09-15 12:44:21 +0430

    Remove redundant default code

--- Assets/UbiRock/Wakizashi/Scripts/Line.cs
+++ Assets/UbiRock/Wakizashi/Scripts/Line.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Line : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+namespace UbiRock.Toolkit {
+    public class Line {
         
     }
 }
