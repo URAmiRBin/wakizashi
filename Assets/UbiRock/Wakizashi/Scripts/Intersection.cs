45204499631cd102c7e2d02e58ec489c15146937
Amir Bin <ur.amirbin@gmail.com>
2021-09-21 21:44:37 +0430

    Add intersection status class

---
+++ Assets/UbiRock/Wakizashi/Scripts/Intersection.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace UbiRock.Wakizashi.Toolkit {
+    public class Intersection {
+        private Tri[] topTriangles, bottomTriangles;
+        private int topTrianglesIndex, bottomTrianglesIndex;
+
+        public Intersection() {
+            topTriangles = new Tri[2];
+            bottomTriangles = new Tri[2];
+            topTrianglesIndex = bottomTriangles = 0;
+        }
+
+        public void AddTopTri(Tri tri) => topTriangles[topTrianglesIndex++] = tri;
+        public void AddBottomTri(Tri tri) => bottomTriangles[bottomTrianglesIndex++] = tri;
+    }
+}
