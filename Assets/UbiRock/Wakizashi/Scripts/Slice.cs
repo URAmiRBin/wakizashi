042791361d17b4454ed53f3672830fca6ae0ca02
Amir Bin <ur.amirbin@gmail.com>
2021-09-22 08:28:57 +0330

    Add slice result

---
+++ Assets/UbiRock/Wakizashi/Scripts/Slice.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace UbiRock.Wakizashi.Toolkit {
+    public class Slice {
+        private Mesh _topMesh, _bottomMesh;
+
+        public Slice(Mesh topMesh, Mesh bottomMesh) {
+            _topMesh = topMesh;
+            _bottomMesh = bottomMesh;
+        }
+
+        public GameObject CreateTopMesh(Transform refereceTransform) {
+            return CreateTopMesh(refereceTransform.localPosition);
+        }
+
+        public GameObject CreateBottomMesh(Transform refereceTransform) {
+            return CreateBottomMesh(refereceTransform.localPosition);
+        }
+
+        private GameObject CreateTopMesh(Vector3 position) {
+            return CreateObjectAt(_topMesh, position);
+        }
+
+        private GameObject CreateBottomMesh(Vector3 position) {
+            return CreateObjectAt(_bottomMesh, position);
+        }
+
+        private GameObject CreateObjectAt(Mesh mesh, Vector3 position, string name = "generated mesh") {
+            GameObject obj = new GameObject(name);
+
+            object.AddComponent<MeshRenderer>();
+            MeshFilter meshFilter = object.AddComponent<MeshFilter>();
+
+            meshFilter.mesh = mesh;
+
+            return obj;
+        }
+    }
+}
