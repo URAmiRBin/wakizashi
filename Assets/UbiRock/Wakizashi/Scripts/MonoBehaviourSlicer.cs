d88f3281d8f7beea354aa4192b38d61813ca0275
Amir Bin <ur.amirbin@gmail.com>
2021-09-25 09:09:36 +0330

    Add interface to the toolkit

---
+++ Assets/UbiRock/Wakizashi/Scripts/MonoBehaviourSlicer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UbiRock.Wakizashi.Toolkit;
+
+namespace UbiRock.Wakizashi {
+    public class MonoBehaviourSlicer : MonoBehaviour {
+        public GameObject meshToSlice;
+        public void Slice() {
+            Plane p = new Plane(Vector3.zero, Vector3.up);
+            SlicedHull result = Slicer.Slice(meshToSlice, p);
+            GameObject topObject = result.CreateTopMesh(transform);
+            GameObject bottomObject = result.CreateBottomMesh(transform);
+
+            Instantiate(topObject, transform.position);
+            Instantiate(bottomObject. transform.position);
+        }
+    }
+}
