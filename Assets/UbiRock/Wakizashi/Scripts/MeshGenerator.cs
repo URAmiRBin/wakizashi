43f3dc7fd15fbb81106d553a8e62272bed2f9f75
Amir Bin <ur.amirbin@gmail.com>
2021-09-25 12:18:59 +0330

    Add triangles to mesh data

--- Assets/UbiRock/Wakizashi/Scripts/MeshGenerator.cs
+++ Assets/UbiRock/Wakizashi/Scripts/MeshGenerator.cs
@@ -7,12 +7,18 @@ namespace UbiRock.Wakizashi.Toolkit {
             Mesh result = new Mesh();
 
             Vector3[] vertecies = new Vector3[triangles.Count * 3];
+            int[] indices = new int[triangles.Count * 3];
 
             for(int i = 0; i < triangles.Count; i += 3) {
                 (vertecies[i], vertecies[i + 1], vertecies[i + 2]) = triangles[i / 3].GetPositions();
+
+                indices[i] = i;
+                indices[i + 1] = i + 1;
+                indices[i + 2] = i + 2;
             }
 
             result.vertices = vertecies;
+            result.SetTriangles(indices, 0, false);
             return result;
         }
     }
