cc803f18ede5f73d020f67b9b11cdf06e0b1e98c
Amir Bin <ur.amirbin@gmail.com>
2021-09-27 12:14:09 +0330

    Fix fill not filling the empty surface

--- Assets/UbiRock/Wakizashi/Scripts/MeshGenerator.cs
+++ Assets/UbiRock/Wakizashi/Scripts/MeshGenerator.cs
@@ -8,8 +8,7 @@ namespace UbiRock.Wakizashi.Toolkit {
 
             Vector3[] vertecies = new Vector3[triangles.Count * 3 + fillTriangles.Count * 3];
             Vector3[] normals = new Vector3[triangles.Count * 3 + fillTriangles.Count * 3];
-            int[] indices = new int[triangles.Count * 3];
-            int[] indicesFill = new int[fillTriangles.Count * 3];
+            int[] indices = new int[triangles.Count * 3 + fillTriangles.Count * 3];
 
             for(int i = 0; i < triangles.Count * 3; i += 3) {
                 (vertecies[i], vertecies[i + 1], vertecies[i + 2]) = triangles[i / 3].GetPositions();
@@ -25,16 +24,14 @@ namespace UbiRock.Wakizashi.Toolkit {
                 (vertecies[i + triangles.Count * 3], vertecies[i + 1 + triangles.Count * 3], vertecies[i + 2 + triangles.Count * 3]) = fillTriangles[i / 3].GetPositions();
                 (normals[i + triangles.Count * 3], normals[i + 1 + triangles.Count * 3], normals[i + 2 + triangles.Count * 3]) = fillTriangles[i / 3].GetNormals();
 
-                indicesFill[i] = i + triangles.Count * 3;
-                indicesFill[i + 1] = i + triangles.Count * 3 + 1;
-                indicesFill[i + 2] = i + triangles.Count * 3 + 2;
+                indices[i + triangles.Count * 3] = i + triangles.Count * 3;
+                indices[i + triangles.Count * 3 + 1] = i + triangles.Count * 3 + 1;
+                indices[i + triangles.Count * 3 + 2] = i + triangles.Count * 3 + 2;
             }
 
             result.vertices = vertecies;
             result.normals = normals;
-            result.subMeshCount = 2;
             result.SetTriangles(indices, 0, false);
-            result.SetTriangles(indicesFill, 1, false);
 
             return result;
         }
