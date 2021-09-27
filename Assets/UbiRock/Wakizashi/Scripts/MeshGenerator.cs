7d65ba083cc937e7bf21a856a702bfe3291bc2de
Amir Bin <ur.amirbin@gmail.com>
2021-09-27 15:43:42 +0330

    Add fill triangles to another submesh

--- Assets/UbiRock/Wakizashi/Scripts/MeshGenerator.cs
+++ Assets/UbiRock/Wakizashi/Scripts/MeshGenerator.cs
@@ -20,20 +20,26 @@ namespace UbiRock.Wakizashi.Toolkit {
                 indices[i + 2] = i + 2;
             }
 
+            int[] indicesFill = new int[fillTriangles.Count * 3];
+
             for(int i = 0; i < fillTriangles.Count * 3; i += 3) {
                 (vertecies[i + triangles.Count * 3], vertecies[i + 1 + triangles.Count * 3], vertecies[i + 2 + triangles.Count * 3]) = fillTriangles[i / 3].GetPositions();
                 normals[i + triangles.Count * 3] = normals[i + 1 + triangles.Count * 3] = normals[i + 2 + triangles.Count * 3] = isTop ? Vector3.down : Vector3.up;
 
                 int start = isTop ? 2 : 0;
                 int inc = isTop ? -1 : 1;
-                indices[i + triangles.Count * 3] = i + triangles.Count * 3 + start;
-                indices[i + triangles.Count * 3 + 1] = i + triangles.Count * 3 + start + inc;
-                indices[i + triangles.Count * 3 + 2] = i + triangles.Count * 3 + start + inc + inc;
+                indicesFill[i] = i + triangles.Count * 3 + start;
+                indicesFill[i + 1] = i + triangles.Count * 3 + start + inc;
+                indicesFill[i + 2] = i + triangles.Count * 3 + start + inc + inc;
             }
 
             result.vertices = vertecies;
             result.normals = normals;
+            result.subMeshCount = 2;
             result.SetTriangles(indices, 0, false);
+            result.SetTriangles(indicesFill, 1, false);
+
+            // WRITE ABOOT THIS
             result.Optimize();
 
             return result;
