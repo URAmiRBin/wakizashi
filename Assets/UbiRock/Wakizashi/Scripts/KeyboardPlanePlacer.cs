3c2273aea7dc876789b81d4e2acbb54163809f5f
Amir Bin <ur.amirbin@gmail.com>
2021-10-05 17:29:17 +0330

    Add sphere to main scene

--- Assets/UbiRock/Wakizashi/Scripts/KeyboardPlanePlacer.cs
+++ Assets/UbiRock/Wakizashi/Scripts/KeyboardPlanePlacer.cs
@@ -24,27 +24,15 @@ public class KeyboardPlanePlacer : UISlicer {
         }
     }
 
-    bool m_Started;
     void FindAndSlice()
     {
-        m_Started = true;
         Collider[] hitColliders = Physics.OverlapBox(_planeTransform.position, _size, _planeTransform.rotation, _layer);
         slicer.SetSlicePlane(_planeTransform);
 
         foreach(Collider collider in hitColliders) {
-            Debug.Log(collider.gameObject.name);
             slicer.meshToSlice = collider.gameObject;
             slicer.Slice();
         }
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
-        if (m_Started)
-            //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
-            Gizmos.DrawWireCube(_planeTransform.position, _size);
     }
 
     public override void Activate(bool value) {
