877c2c5fc48be585c9996f44b20a80c5c1ae65ca
Amir Bin <ur.amirbin@gmail.com>
2021-10-05 13:59:56 +0330

    Add scroller component

---
+++ Assets/UbiRock/Wakizashi/Scripts/Scroller.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroller : MonoBehaviour {
+    [SerializeField] string[] items;
+    Animator _animator;
+    Text _nowText, _nextText;
+    int _currentItemIndex;
+    WaitForEndOfFrame _frameBreather = new WaitForEndOfFrame();
+
+    void Awake() {
+        _animator = GetComponent<Animator>();
+        _nowText = transform.GetChild(0).GetComponent<Text>();
+        _nextText = transform.GetChild(1).GetComponent<Text>();
+    }
+
+    void Start() {
+        _nowText.text = items[_currentItemIndex];
+        _nextText.text = items[(_currentItemIndex + 1) % items.Length];
+    }
+
+    public void Scroll() => _animator.SetTrigger("scroll");
+
+    public void UpdateScrollText() => StartCoroutine(UpdateScrollTextCoroutine());
+
+    IEnumerator UpdateScrollTextCoroutine() {
+        yield return _frameBreather;
+        _currentItemIndex = (_currentItemIndex + 1) % items.Length;
+        _nowText.text = items[_currentItemIndex];
+        _nextText.text = items[(_currentItemIndex + 1) % items.Length];
+    }
+}
--- Assets/UbiRock/Wakizashi/Scripts/SliceController.cs
+++ Assets/UbiRock/Wakizashi/Scripts/SliceController.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
 public class SliceController : MonoBehaviour {
+    [SerializeField] Scroller _scroller;
     UISlicer[] _slicers;
     int _currentSlicerIndex = 0;
 
     void ChooseNextSlicer() => _currentSlicerIndex = _currentSlicerIndex >= _slicers.Length - 1 ? 0 : _currentSlicerIndex + 1;
-    void ChoosePreviousSlicer() => _currentSlicerIndex = _currentSlicerIndex <= 0 ? _slicers.Length - 1 : _currentSlicerIndex - 1;
-
+    
     public void StartSlice() {
         _slicers[_currentSlicerIndex].SetActive(true);
     }
