591c48f3831e95e08ef52870e0245c23a50d30f2
Amir Bin <ur.amirbin@gmail.com>
2021-10-05 16:43:57 +0330

    Add plane cutter

--- Assets/UbiRock/Wakizashi/Scripts/KeyboardPlanePlacer.cs
+++ Assets/UbiRock/Wakizashi/Scripts/KeyboardPlanePlacer.cs
@@ -1,5 +1,26 @@
-public class KeyboardPlanePlacer : UISlicer
-{
+using UnityEngine;
+
+public class KeyboardPlanePlacer : UISlicer {
     bool _isActive = false;
-    public override void Activate(bool value) => _isActive = true;
+    GameObject _plane;
+    Transform _planeTransform;
+
+    void Awake() {
+        _planeTransform = transform.GetChild(0);
+        _plane = _planeTransform.gameObject;
+    }
+
+    void Update() {
+        if (!_isActive) return;
+        if (Input.GetMouseButtonDown(0)) {
+            // TODO: CUT
+        } else {
+            _planeTransform.RotateAround(transform.position, transform.forward, Input.mouseScrollDelta.y);
+        }
+    }
+
+    public override void Activate(bool value) {
+        _isActive = value;
+        _plane.SetActive(value);
+    }
 }
